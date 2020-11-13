b6d8c65f1a782478c42f9992e54831a85c0ddea9
Tunji Akanbi <you@example.com>
2020-11-13 17:55:13 -0600

    started adding text animations

--- js/app.js
+++ js/app.js
@@ -27,7 +27,15 @@ $(document).ready(function () {
     let miiscrect2 = $('#miisc>.cls-4');
     let miiscrect3 = $('#miisc>.cls-3');
     let miiscrect4 = $('#miisc>.cls-2');
-    let miiscrect5 = $('#miisc>.cls-1');
+    let miisccirc5 = $('#miisc>.cls-1');
+
+    let bluerect = $('#blueBase>.cls-2');
+    let headText = $('#heading-text1>path')
+
+    const ei = "power4.in";
+    const eo = "power4.out";
+    const bo = "bounce.out";
+    const bi = "bounce.in";
 
     function intro() {
         let whitebkgdTl = gsap.timeline({
@@ -40,7 +48,8 @@ $(document).ready(function () {
             }, 1000, () => {
                 whitebkgdTl.fromTo(rect1, {
                     autoAlpha: 0,
-                    x: 100
+                    x: 100,
+                    ease: bo
                 }, {
                     autoAlpha: 1,
                     x: 0,
@@ -55,26 +64,98 @@ $(document).ready(function () {
             id: "step1"
         })
         step1.to(miisc, {
-            autoAlpha: 1, duration: 1
+            autoAlpha: 1,
+            duration: 1
         })
-        step1.from(miiscrect3,{autoAlpha: 0, x:-600, duration:1})
-        step1.from(miiscrect4,{autoAlpha: 0, y:-400, duration:1})
+        step1.from(miiscrect3, {
+            autoAlpha: 0,
+            x: -600,
+            ease: bi,
+            duration: 1
+        })
+        step1.from(miiscrect4, {
+            autoAlpha: 0,
+            y: -400,
+            ease: bo,
+            duration: 1
+        })
         return step1
     }
+
     function step_2() {
         let step2 = gsap.timeline({
             id: "step2"
         })
         step2.from(miiscrect2, {
-            autoAlpha: 0, stagger: 0.3
+            autoAlpha: 0,
+            x: 50,
+            y: 50,
+            stagger: 0.15,
+            ease: bi
         })
         return step2
     }
 
+    function step_3() {
+        let step3 = gsap.timeline({
+            id: "step3"
+        })
+
+        step3.fromTo(blueBase, {
+            autoAlpha: 0,
+            x: 50,
+            y: 50,
+            ease: bi
+        }, {
+            autoAlpha: 1,
+            duration: 2
+        }, "+=1")
+
+        step3.fromTo(bluerect, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: bi,
+            stagger: 0.4
+        })
+        return step3
+    }
+
+    function step_4() {
+        let step4 = gsap.timeline({
+            id: "step4"
+        })
+        step4.from(miisccirc5, {
+            autoAlpha: 0,
+            x: 150,
+            y: 150,
+            stagger: 0.3,
+            ease: bo
+        })
+        return step4
+    }
+    function step_5(){
+        let step5 = gsap.timeline({
+            id: "step5"
+        })
+        step5.fromTo(headingtext1,{autoAlpha:0},{autoAlpha:1})
+        step5.from(headText, {
+            autoAlpha: 0,
+            x: -15,
+            y: 10,
+            stagger: 0.1,
+            ease: bi
+        })
+        return step5
+    }
+
     let master = gsap.timeline();
-    master.add(intro(),"+=0.5")
-        .add(step_1(),"+=0.5")
-        .add(step_2(),"-=1")
+    master.add(intro(), "+=0.5")
+        .add(step_1(), "+=0.5")
+        .add(step_2(), "-=0.25")
+        .add(step_3(), "-=2")
+        .add(step_4(), "-=1")
+        .add(step_5(), "-=2")
     GSDevTools.create();
     //document.ready ends here
 });
